ae634644a02cbd861ecfa9e17f63d764aa3e6aa7
zju-xjh <1252736593@qq.com>
2021-10-09 16:23:08 +0800

    first commit

---
+++ src/main/java/com/test/demo/controller/TestBootController.java
@@ -0,0 +1,16 @@
+package com.test.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/test")
+public class TestBootController {
+
+    @RequestMapping("demo")
+    public String getString() {
+        return "success";
+    }
+}
